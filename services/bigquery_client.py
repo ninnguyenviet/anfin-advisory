48dd4f8fd819bbbfa418f836b3b7cf35a911f2a4
Admin <admin@Admins-MacBook-Pro.local>
2025-08-26 11:45:32 +0700

    update user request

--- services/bigquery_client.py
+++ services/bigquery_client.py
@@ -27,7 +27,7 @@ def load_account_requests():
     expanded = df_raw["data"].apply(lambda x: json.loads(x))
     expanded_df = pd.json_normalize(expanded)
     expanded_df.columns = [col.replace(".", "_") for col in expanded_df.columns]
-    expanded_df = expanded_df[["avatar_url", "bio", "display_name", "highlights","source", "phone_number"]]
+    expanded_df = expanded_df[["avatar_url", "bio", "display_name", "highlights", "phone_number"]]
     df = pd.concat([df_raw.drop(columns=["data"]), expanded_df], axis=1)
     df.sort_values(by="created_at", ascending=False, inplace=True)
     return df
